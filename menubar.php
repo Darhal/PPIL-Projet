e6e67d7ae05b66f1cea463585a5f42f7b9d17998
Nada <madadnada@gmail.com>
2020-02-27 11:26:34 +0100

    Profile page update

---
+++ menubar.php
@@ -0,0 +1,30 @@
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+<ul>
+    <li><a class="active" href="#home">Acceuil</a></li>
+    <li><a href="#">Profile</a></li>
+    <li><a href="#liste">Mes listes</a></li>
+    <li><a href="#Contribution">Contribution</a></li>
+    <li><a href="#logout">Deconnexion</a></li>
+    <li><a href="#about">About</a></li>
+
+</ul>
+<!-- Search Wrapper -->
+<div class="search-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <form action="#" method="post">
+                    <input type="search" name="search" placeholder="Recherche personnes..">
+                    <button type="submit"><i class="fa fa-search" aria-hidden="true">Ok</i></button>
+                    <a href="#" class="notification">
+                        <span>Notification</span>
+                        <span class="badge">3</span>
+                    </a>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</nav>
--- profile.php
+++ profile.php
@@ -15,73 +15,8 @@ $unwanted_array = array(
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="">
+    <link rel="stylesheet" href="style.css">
 	<style type="text/css">
-	body{
-		background: #F0FFFF;
-	}
-	form {
-		//width:10%;
-		padding: 3px;
-		border: 1px solid #f1f1f1;
-		background: #fff;
-		box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
-		color: #008B8B ;
-        border-radius: 20px;
-	}
-    form button{
-        border: none;
-        outline: 0;
-        display: inline-block;
-        padding: 5px;
-        color: white;
-        background-color: #555;
-        width: 5%;
-        font-size: 15px;
-        border-radius: 20px;
-    }
-    .notification {
-        background-color: #555;
-        color: white;
-        text-decoration: none;
-        padding: 8px 16px;
-        position: relative;
-        display: inline-block;
-        left: 700px;
-        border-radius: 2px;
-    }
-    .notification:hover {
-        background: red;
-    }
-    .notification .badge {
-        position: absolute;
-        top: -10px;
-        right: -10px;
-        padding: 5px 10px;
-        border-radius: 50%;
-        background-color: red;
-        color: white;
-    }
-    ul {
-        background-color: #555;
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-    }
-    li {
-        float: left;
-    }
-    li a {
-        display: block;
-        color: white;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-    }
-
-    li a:hover {
-        background-color: #111;
-    }
     .card {
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
         max-width: 300px;
@@ -94,8 +29,7 @@ $unwanted_array = array(
         color: grey;
         font-size: 18px;
     }
-
-    button {
+    p input{
         border: none;
         outline: 0;
         display: inline-block;
@@ -107,44 +41,10 @@ $unwanted_array = array(
         width: 100%;
         font-size: 18px;
     }
-    a {
-        text-decoration: none;
-        font-size: 22px;
-        color: black;
-    }
-
 	</style>
-
 </head>
-
 <body>
-
-<ul>
-    <li><a class="active" href="#home">Acceuil</a></li>
-    <li><a href="#">Profil</a></li>
-    <li><a href="#contact">Contact</a></li>
-    <li><a href="#about">About</a></li>
-</ul>
-
-
-<!-- Search Wrapper -->
-<div class="search-wrapper">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <form action="#" method="post">
-                    <input type="search" name="search" placeholder="Recherche personnes..">
-                    <button type="submit"><i class="fa fa-search" aria-hidden="true">Ok</i></button>
-                    <a href="#" class="notification">
-                        <span>Notification</span>
-                        <span class="badge">3</span>
-                    </a>
-
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+<?php include "./menubar.php" ?>
 <h2 style="text-align:center"></h2>
 <div class="card">
     <img src="img/index.png" style="width:100%">
@@ -153,9 +53,8 @@ $unwanted_array = array(
     <p >Departement name</p>
     <p>Email : </p>
     <div style="margin: 24px 0;">
-    <p><button>Modifier profile</button></p>
+        <p><input type="button" onclick="location.href='editprofile.php';" value="Modifier profile" /></p>
+
 </div>
-
-
 </body>
 </html>
