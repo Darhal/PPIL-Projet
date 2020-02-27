e91cad541641301addc02376dc15ec3f2a3e028c
Ugo Cottin <ugo.cottin@gmail.com>
2020-02-27 11:50:08 +0100

    ajout de la classes Systeme
    Signed-off-by: ugocottin <ugo.cottion@gmail.com>

--- Backend/Systeme.php
+++ Backend/Systeme.php
@@ -1,7 +1,14 @@
 <?php
 
+include_once "Utilisateur.php";
 
 class Systeme
 {
+    public function ajouterUtilisateur(Utilisateur $utilisateur) {
 
+    }
+
+    public function supprimerUtilisateur(int $utilisateurID) {
+
+    }
 }
