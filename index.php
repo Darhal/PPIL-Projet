c7540dc527842f925e410b1cb4648451517ccc6b
Jawtoch <ugo.cottin@gmail.com>
2020-02-27 09:43:30 +0100

    Ajout de la page d'accueil et du style
    Signed-off-by: ugocottin <ugo.cottion@gmail.com>

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Procrast - Accueil</title>
+</head>
+<body>
+
+    <div class="spacer"></div>
+    <h1 class="text-center"> Procrast </h1>
+    <div class="spacer"></div>
+
+    <div class="text-center">
+        <div class="d-flex-centered-row">
+            <p> Déjà membre ? </p>
+            <button> Se connecter </button>
+        </div>
+        <div class="d-flex-centered-row">
+            <p> Pas encore inscrit ? </p>
+            <button> S'inscrire </button>
+        </div>
+    </div>
+</body>
+</html>
