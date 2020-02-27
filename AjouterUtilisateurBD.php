48fe050ca9fb310b360fc0b13e91c1cfc6e268b8
Lothary <nicolas.bombarde@free.fr>
2020-02-27 11:44:15 +0100

    Inscription A DEUX DOIGTS DE FONCTIONNER Si si je vous jure

--- AjouterUtilisateurBD.php
+++ AjouterUtilisateurBD.php
@@ -2,37 +2,28 @@
 
 // Connexion
 try {
-	$bd = new SQLite3('BD.sqlite');
+	$bd = new SQLite3('BD.sqlite', 0666);
 } catch (SQLiteException $e) {
 	die("La création ou l'ouverture de la base a échouée ".
 		"pour la raison suivante: ".$e->getMessage());
 }
 
-$login = $_POST['login'];
 
-$result = $bd->query('SELECT * FROM utilisateur WHERE pseudo = "'.$login.'";');
-$data = $result-> fetchArray();
-
-if($data['id'] != "" ){		#Un compte avec ce pseudo existe déjà
-	header('location: ../Inscription.php?erreur=1');
-}
-else{		#Le compte n'existe pas encore, création du compte
-
-	if ($_POST['login'] != '' AND $_POST['password'] != '' ) { //Si les champs ne sont pas vides
+if ($_POST['login'] != '' AND $_POST['password'] != '' ) { //Si les champs ne sont pas vides
 	$login = $_POST['login'];
 	$password = $_POST['password'];
 	$nom = $_POST['nom'];
 	$prenom  = $_POST['prenom'];
 	$mail = $_POST['mail'];
-
-    $bd->query('INSERT INTO utilisateur VALUES (NULL, "'.$login.'","'.$nom.'","'.$prenom.'","'.$mail.'","'.$password.'");');
-	#   INSERT INTO utilisateur VALUES (NULL, 'louy','louy','louy','louy','louy');
+    echo(sprintf("INSERT INTO utilisateur VALUES (NULL, '%s','%s','%s','%s','%s');", $login, $nom, $prenom, $mail, $password));
+    #$bd->exec("INSERT INTO utilisateur VALUES (NULL, 'test','test','test','test','test');");
+    $bd->query(sprintf("INSERT INTO utilisateur VALUES (NULL, '%s','%s','%s','%s','%s');", $login, $nom, $prenom, $mail, $password));
 
 	// Revenir à la page principale avec le compte de l'utilisateur à présent connecté
 	#$_SESSION['login'] = $_POST['login'];
-	header('location: index.html');
+	#header('location: index.html');
 
-	}
+
 
 }
 
