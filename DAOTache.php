c4b197adf66143a33c3af40b7911fc579e1e25d3
Buiai <louymasset@hotmail.fr>
2020-02-27 12:01:33 +0100

    -Ajout de DAOUtilisateur.php à compléter plus tard et à corriger

---
+++ DAOTache.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class DAOTache extends DAO
+{
+    public function __construct()
+    {
+    }
+
+    public function ajouterDansBDD($objet)
+    {
+        // TODO: Implement ajouterDansBDD() method.
+    }
+
+    public function supprimerDeBDD($objet)
+    {
+        // TODO: Implement supprimerDeBDD() method.
+    }
+
+    public function getByRequete($requete)
+    {
+        // TODO: Implement getByRequete() method.
+    }
+
+    public function getBDD()
+    {
+        // TODO: Implement getBDD() method.
+    }
+}
