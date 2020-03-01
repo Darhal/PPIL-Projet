a5aee5ffb127d4eee8e247086f23a434e4d96a16
jpierrel <jonathan.pierrel@gmail.com>
2020-03-01 21:36:42 +0100

    Systeme.php fonction ajouterUtilisateur()  -- qu'est-ce qu'il faut passer en sessionID ?

--- Backend/Systeme.php
+++ Backend/Systeme.php
@@ -4,7 +4,35 @@ include_once "Utilisateur.php";
 
 class Systeme
 {
-    public function ajouterUtilisateur(Utilisateur $utilisateur) {
+    private $arrayUtilisateurs = array();  // liste des Utilisateurs
+
+
+    function __construct()
+    {
+
+    }
+
+    function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+    }
+
+    public function ajouterUtilisateurInstance(Utilisateur $utilisateur) {
+
+    }
+    public function ajouterUtilisateur(string $pseudo, string $prenom, string $nom, string $email, string $mdp) {
+
+        $nouvelUtilisateur = new Utilisateur();  // c'est quoi le SessionID?
+        $nouvelUtilisateur->setPseudo($pseudo);
+        $nouvelUtilisateur->setPrenom($prenom);
+        $nouvelUtilisateur->setNom($nom);
+        $nouvelUtilisateur->setEmail($email);
+        $nouvelUtilisateur->setMdp($mdp);
+
+
+        array_push($this->arrayUtilisateurs, $nouvelUtilisateur);
+
+        //Ajouter l'utilisateur à la BDD
 
     }
 
