8811c3dac0f5b450b9a8477c40f776b0f1eadc07
battikh1u <linda.battikh4@etu.univ-lorraine.fr>
2020-03-02 19:09:35 +0100

    notification2.0

--- notification.php
+++ notification.php
@@ -16,13 +16,23 @@
     <div class="row">
         <div class="col-md-7">
             Notifications
+            <div class="col-md-7">
+                notification1
+            </div>
+            <div class="col-md-7">
+                notification2
+            </div>
         </div>
         <div class="col-md-2">
             Supprimer
+            <div class="col-md-7">
+                <img src="/Assets/Images/icons/trash.svg" alt="" width="32" height="32">
+            </div>
         </div>
     </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
+<svg class="bi bi-chevron-right" width="32" height="32" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z" clip-rule="evenodd"/></svg>
 </body>
 </html>
