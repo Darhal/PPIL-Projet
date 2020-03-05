29e3a5ffa217a07c5b40354215de82fe1350b08f
arshiamir <arshiamir92@gmail.com>
2020-03-05 14:53:49 +0100

    completer le corps des classes Invitation.php et ses extensions

--- Backend/Invitation/Invitation.php
+++ Backend/Invitation/Invitation.php
@@ -1,8 +1,21 @@
 <?php
 
 abstract class Invitation {
+    public $msg;
+    public $inviteur;
+    public $liste;
+    public $idInvitation;
+    function __construct(string $message,int $idInviteur, int $idListe)
+    {
+        $this->msg=$message;
+        $this->inviteur = $idInviteur;
+        $this->liste=$idListe;
 
-    protected $msg;
+    }
+    function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+    }
 }
 
 ?>
--- Backend/Invitation/InvitationTransfererPropriete.php
+++ Backend/Invitation/InvitationTransfererPropriete.php
@@ -2,6 +2,10 @@
 include_once "Invitation.php";
 
 class InvitationTransfererPropriete extends Invitation {
+    function __construct(string $message, int $idInviteur, int $idListe)
+    {
+        parent::__construct($message, $idInviteur, $idListe);
+    }
 
 }
 
