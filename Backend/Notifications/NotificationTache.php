852069b6660f53232b78f887948d42768fb3d293
arshiamir <arshiamir92@gmail.com>
2020-03-05 14:57:32 +0100

    modifications NotificationListeTaches.php et NotificationTache.php

--- Backend/Notifications/NotificationTache.php
+++ Backend/Notifications/NotificationTache.php
@@ -1,7 +1,5 @@
 <?php
 include_once "Notification.php";
-include_once getenv('BASE')."Backend/Taches/Tache.php";
-include_once getenv('BASE')."Backend/Taches/ListeTaches.php";
 
 class NotificationTache extends Notification {
     public $tache;
@@ -16,40 +14,8 @@ class NotificationTache extends Notification {
         parent::__destruct();
     }
 
-    public function getListeTaches()
-    {
-        return $this->listeTaches;
-    }
-
-    public function getMsg()
-    {
-        return $this->msg;
-    }
-
-    public function setMsg(string $msg): void
-    {
-        $this->msg = $msg;
-    }
-
     function marquerCommeLu(){
         $this->dejaLu=true;
-    }
-
-    function estLu(){
-        return $this->dejaLu;
-    }
-
-    function setID(int $id){
-        $this->idNotif=$id;
-    }
-
-    function getID(){
-        return $this->idNotif;
-    }
-
-    public function getTache()
-    {
-        return $this->tache;
     }
 
 
