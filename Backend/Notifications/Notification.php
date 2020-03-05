4a412d5f6692cd793ff49f7e52330582390d3bd3
arshiamir <arshiamir92@gmail.com>
2020-03-05 14:44:17 +0100

    completer le corps des classes Notification.php et ses extensions

--- Backend/Notifications/Notification.php
+++ Backend/Notifications/Notification.php
@@ -1,8 +1,20 @@
 <?php
 
 abstract class Notification {
-    private $msg;
-    private $dejaLu;
+    public $msg;
+    public $dejaLu;
+    public $listeTaches;
+    public $idNotif;
+    function __construct(string $message,bool $lu,int $liste)
+    {
+        $this->dejaLu=$lu;
+        $this->msg = $message;
+        $this->listeTaches=$liste;
+    }
+    function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+    }
 }
 
 ?>
