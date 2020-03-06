35cd34394f2f2cb3b89e4d2fe9476301b6f0d58a
ugocottin <ugo.cottion@gmail.com>
2020-03-06 10:51:36 +0100

    fix de la page d'accueil

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="/style.css">
+	<title>Procrast - Accueil</title>
+</head>
+<body>
+<?php include_once "Shared/navbar.php"; ?>
+<div class="spacer"></div>
+<h1 class="text-center"> Procrast </h1>
+<div class="spacer"></div>
+
+<div class="text-center">
+	<div class="d-flex-centered-row">
+		<p> Déjà membre ? </p>
+		<button onclick="window.location.href='/Frontend/Login'"> Se connecter </button>
+	</div>
+	<div class="d-flex-centered-row">
+		<p> Pas encore inscrit ? </p>
+		<button onclick="window.location.href='Frontend/Signup'"> S'inscrire </button>
+	</div>
+</div>
+<?php include_once "Shared/footer.php"; ?>
+</body>
+</html>
