cd5a6c16f3d576a2bb11ab73bee2056aaee1c490
Lothary <nicolas.bombarde@free.fr>
2020-03-19 10:17:45 +0100

    fix page signup

--- Frontend/Signup/signup.php
+++ Frontend/Signup/signup.php
@@ -5,29 +5,30 @@ include_once "Backend/Utilisateur/Systeme.php";
 
 Systeme::start_session();
 
-if ($_POST['pseudo'] != '' AND $_POST['prenom'] != '' AND $_POST['nom'] != '' AND $_POST['email'] != '' AND $_POST['password'] != '' AND $_POST['conf-password']!='') { //Si les champs ne sont pas vides
+if ($_POST['pseudo'] != '' AND $_POST['prenom'] != '' AND $_POST['nom'] != '' AND $_POST['email'] != '' AND $_POST['password'] != '') { //Si les champs ne sont pas vides
     Systeme::Init();
+
+    if($_POST['password'] != $_POST['conf-password']){
+        header("location: ../Signup/index.php?erreur=3");
+        exit;
+    }
 
     // TODO: - Protéger contre l'injection SQL
     $user = new Utilisateur($_POST['pseudo'], $_POST['prenom'], $_POST['nom'], $_POST['email'], $_POST['password']);
     $err_code = Systeme::ajouterUtilisateur($user);
 
     if ($err_code) {
-        header("location: ../Signup/index.php?erreur=".$err_code);  //email existe déjà code erreur 1
-        exit;
-    }
-    if ($_POST['password'] != $_POST['conf-password']) {
-        header("location: ../Signup/index.php?erreur=3");
+        header("location: ../Signup/index.php?erreur=".$err_code);
         exit;
     }
 
     if (Systeme::seConnecter($user->email, $user->mdp)){
         // Redirection vers la page d'accueil
-		header("location: ../Profil");   // Revenir à la page principale avec le compte de l'utilisateur à présent connecté
+        header("location: ../Profil");   // Revenir à la page principale avec le compte de l'utilisateur à présent connecté
+        exit;
     }
-}
-
-else{   //Si les informations ne sont pas remplies
+} else {
+    //Si les informations ne sont pas remplies
     header("location: ../Signup/index.php?erreur=2");
-	exit;
+    exit;
 }
