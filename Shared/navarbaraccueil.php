708308db4900c7c9506894e87165991d92e7ce39
Omar <chidaomar46@gmail.com>
2020-03-19 15:40:50 +0100

    Fixed absolute paths in navbaraccueil.php et navbar.php

--- Shared/navarbaraccueil.php
+++ Shared/navarbaraccueil.php
@@ -13,8 +13,8 @@ $account = $target;
 
 if (Systeme::estConnecte()) {
     $text = "Déconnexion";
-    $target = "/Frontend/Login/logout.php";
-    $account = "/Frontend/Profil";
+    $target = "../Frontend/Login/logout.php";
+    $account = "../Frontend/Profil";
 
 
 }
@@ -22,7 +22,7 @@ if (Systeme::estConnecte()) {
 echo '
 <nav class="site-header sticky-top py-1">
 	<div class="align-center d-flex flex-column flex-md-row justify-content-between" style="max-width: 80%">
-		<a class="py-1 d-none" href="/index.php">Accueil</a>
+		<a class="py-1 d-none" href="../index.php">Accueil</a>
 		<a class="py-1 d-none" href="'.$account.'">Mon compte</a>
 		<a class="py-1 d-none" href="'.$target.'">'.$text . '</a>
 	</div>
