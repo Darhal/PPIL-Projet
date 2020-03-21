231d5ef14e236b31cb1f15137c8c73bcb40536a1
Omar <chidaomar46@gmail.com>
2020-03-21 17:45:35 +0100

    Fixed Supprimer.php to get all notifications

--- Frontend/Notification/supprimer.php
+++ Frontend/Notification/supprimer.php
@@ -16,21 +16,23 @@ Systeme::Init();
 $uid = $_SESSION['id'];
 $utilisateur = Systeme::getUserByID($uid);
 
-if (!isset($_GET['id'])) {
+if (!isset($_GET['lid'])) {
 	header( "location: notification.php");
 }
 
 $notifID = intval($_GET['lid']);
 
-$notifications = Systeme::getNotifications($utilisateur);
+$notifTache = Systeme::getNotificationsTache($utilisateur->id) ;
+$notifList = Systeme::getNotificationsListe($utilisateur->id);
+
+$notifications = array_merge($notifTache, $notifList); //fusion des notifs
 
 foreach ($notifications as $notification) {
 	echo "$notification->id | $notifID";
 	if ($notification->id == $notifID) {
 		if(Systeme::supprimerNotification($notification)){
-            header( "location: notification.php");
-        }
-		else{
+            // header( "location: notification.php");
+        }else{
 		    echo "erreur" ;
         }
 	}
