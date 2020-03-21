19393040443c909899d7e35f074babcaa6bc2cca
battikh1u <linda.battikh4@etu.univ-lorraine.fr>
2020-03-21 15:22:34 +0100

    recherche ajout de ajax fin

---
+++ Frontend/Invit/pseudoR.php
@@ -0,0 +1,13 @@
+<?php
+include "../../Backend/Utilisateur/Systeme.php";
+if (isset($_GET["pseudo"])) {
+    $pseuso = $_GET["pseudo"];
+} else {
+    $pseudo = "";
+}
+$systeme= new Systeme;
+$tab=$systeme::getUsersByPseudo($pseudo);
+foreach ($tab as $personne){
+    echo "$personne";
+}
+?>
