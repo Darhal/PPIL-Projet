9ffbb24af41876c58989c41ceeaac034d5d6f9f6
Ugo Cottin <ugo.cottin@gmail.com>
2020-03-23 20:30:40 +0100

    le bouton pour ajouter des utilisateurs n'est visible qu'au propriétaire de la liste
    Signed-off-by: Ugo Cottin <ugo.cottin@gmail.com>

--- Frontend/Lists/View/index.php
+++ Frontend/Lists/View/index.php
@@ -202,15 +202,19 @@ if (!in_array($user, $membres)) {
 		}
 
 		Liste($liste, $user);
-		?>
 
-    <div class="float-right">
-	    <form action="../../Tasks/creer.php" method="post">
-		    <input type="submit" value="Ajouter une tâche">
-		    <label for="lid"></label><input hidden type="text" id="lid" name="lid" value="<?php echo $liste->id; ?>">
+		if ($liste->proprietaire == $user->id) {
+			echo "
+			<div class=\"float-right\">
+	    <form action=\"../../Tasks/creer.php\" method=\"post\">
+		    <input type=\"submit\" value=\"Ajouter une tâche\">
+		    <label for=\"lid\"></label><input hidden type=\"text\" id=\"lid\" name=\"lid\" value=\"<?php echo $liste->id; ?>\">
 	    </form>
     </div>
-
+			
+			";
+		}
+		?>
 </div>
 
 <?php include_once "Shared/footer.php"; ?>
